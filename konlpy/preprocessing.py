b64ff7a1ad515c69507b3e9958e99fec6096f2b1
Jihyun <bjih1999@naver.com>
2020-11-17 00:02:26 +0900

    절 단위로 분리

--- konlpy/preprocessing.py
+++ konlpy/preprocessing.py
@@ -6,10 +6,20 @@ import re
 hannanum = Hannanum()
 
 reviewlist = []
+sentences = []
 with open('konlpy/reviewlist.csv', 'r') as reviewfile:
     reader = csv.reader(reviewfile)
     for row in reader:
         reviewlist.append(row)
+
+    for review in reviewlist:
+        review = review[0]
+        review_entered = review.split('\n')
+
+        for review_splitted in review_entered:
+            sentences.append(review_splitted)
+
+reviewlist = sentences
 
 # print(reviewlist[:10])
 #
@@ -21,12 +31,14 @@ with open('konlpy/reviewlist.csv', 'r') as reviewfile:
 # regex1 = re.compile(r'(^\[)($])')
 # print(re.sub(r'^\[[\w\b]+\]$', '', '[2020-1학기 공과대학 수강후기 공모전]'))
 # print(re.sub(r'(\[)([\w\s-]*)(\])', '', '[2018-2 경영대학 리얼 수강후기 공모전]\n*상단의 과제/조모임/학점비율 등의 항목들은 일괄적으로 가장 왼쪽 항목에 체크되었으며, 아래의 본 내용과 무관함을 알려드립니다.\n*본 후기는 온전히 경영대학 학우 분들의 의견으로 구성되었습니다.\n교수님의 수업 스타일은 매우 자유분방 하십니다. 너무 자유분방하셔서 수업의 본질을 흐리시는 것 같네요 금융시장론이란 명칭을 바꾸셔야 될거 같습니다. 밴드라는 걸 이용해서 매주 기사 스크랩을 시키고 참잘했어요를 주십니다 시험은 기말고사 한번인데 비중이 매우 적습니다 출결은 거의 반영 안되는 것 같구요 성적 비중을 교수님 마음대로 정합니다 팀프로젝트가 한 번 있어요').replace('*상단의 과제/조모임/학점비율 등의 항목들은 일괄적으로 가장 왼쪽 항목에 체크되었으며, 아래의 본 내용과 무관함을 알려드립니다.', '').replace('*본 후기는 온전히 경영대학 학우 분들의 의견으로 구성되었습니다.', ''))
-with open('konlpy/preprocessed_review.csv', 'w', newline='') as output_file:
+with open('konlpy/preprocessed_review_sent.csv', 'w', newline='') as output_file:
+    #test 중
     writer = csv.writer(output_file)
     preprocessed_sent = []
+    # M_check = []
     for review in reviewlist:
-        print(review[0])
-        review = review[0]
+        print(review)
+        # review = review[0]
         review = re.sub(r'(\[)([\w\s-]*)(\])', '', review)  # [20nn-n학기 ~~대학 공모전 ~]과 같은 형식 제거
         review = re.sub(r'(\*+)([\w\s\.\-]*)(\*+)', '', review)  # ex) ** 20-1학기 IT대학 강의평가 공모전을 통해 접수된 강의평가입니다. ** 와 같은 형식 제거
         review = re.sub(r'(\*)([\w\s\/,]*)(.)', '', review)  # ex) *본 후기는 온전히 경영대학 학우 분들의 의견으로 구성되었습니다. 와 같은 형식 제거
@@ -37,14 +49,22 @@ with open('konlpy/preprocessed_review.csv', 'w', newline='') as output_file:
         review = review.replace('ㅎ', '')
         review = review.replace('"', '')
         spaced_review = spacing(review)
-        morphs = hannanum.pos(spaced_review)
+        morphs = hannanum.pos(spaced_review, ntags=22)
+        print(morphs)
         sent=[]
-        for morph in morphs:
-            if morph[1] != 'J' and morph[1] != 'E' and morph[1] != 'S':
+        for morph in morphs: # morph[1].startswith('M') == False and
+            if  morph[1] != 'II' and morph[1].startswith('J') == False and morph[1].startswith('S') == False and morph[1].startswith('E') == False:
                 sent.append(morph[0])
-        preprocessed_sent.append(sent)
-        print(sent)
-        writer.writerow(sent)
+            if morph[1].startswith('S') or (morph[1].startswith('E') and '고' in morph[1]):
+                break;
+            # if morph[1].startswith('M'):
+            #     M_check.append(morph[0])
+
+
+        if len(sent) != 0:
+            preprocessed_sent.append(sent)
+            print(sent)
+            writer.writerow(sent)
 
 
 
