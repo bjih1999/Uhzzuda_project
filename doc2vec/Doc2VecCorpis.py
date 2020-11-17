fc4ea6bd128e6ba2b03aa0f7e84b9faab3647d9c
Jihyun <bjih1999@naver.com>
2020-11-17 16:25:20 +0900

    절 단위 분리

--- doc2vec/Doc2VecCorpis.py
+++
@@ -1,9 +0,0 @@
-from gensim.models.doc2vec import TaggedDocument
-
-class Doc2VecCorpus:
-    def __init__(self, fname):
-        self.fname = fname
-
-    def __iter__(self):
-        with open(self.fname, encoding='utf-8') as file:
-            for doc in file:
