489d1585242c280e9bad3bb7d74298cb0c43458b
Jaehee Park <vito822@naver.com>
2020-12-04 22:42:04 +0900

    튜닝 중

--- jaebal/test_doc2vec.py
+++ jaebal/test_doc2vec.py
@@ -2,12 +2,12 @@
 from gensim.models.doc2vec import Doc2Vec
 print("======학점 숙제 시험 교수 강의======")
 #model = doc2vec_model
-model = Doc2Vec.load('./doc2vec/4.model')
+model = Doc2Vec.load('./doc2vec/8.model')
 print("================= new1 ===================")
-print(model.most_similar('조', topn=30))
-print(model.most_similar('별', topn=30))
-print(model.most_similar('팀', topn=30))
-print(model.most_similar('프로젝트', topn=30))
-print(model.most_similar('학점', topn=30))
-
+print(model.most_similar('빡세', topn=30))
+print(model.most_similar('강', topn=30))
+print(model.most_similar('싸강', topn=30))
+print(model.most_similar('범위', topn=30))
+print(model.most_similar('오프라인', topn=30))
+# print(model.most_similar(positive=['학점'], negative=['F']))
 
