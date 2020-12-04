8ca7207a05065ad4e6c18af1683be951b3f91601
Jaehee Park <vito822@naver.com>
2020-12-05 00:00:56 +0900

    gogogogo

--- jaebal/test_doc2vec.py
+++ jaebal/test_doc2vec.py
@@ -1,13 +1,18 @@
 
 from gensim.models.doc2vec import Doc2Vec
 print("======학점 숙제 시험 교수 강의======")
-#model = doc2vec_model
-model = Doc2Vec.load('./doc2vec/8.model')
+model = Doc2Vec.load('./doc2vec/3.model')
 print("================= new1 ===================")
-print(model.most_similar('빡세', topn=30))
+print(model.most_similar('강', topn=30))
+print(model.most_similar('싸', topn=30))
+print(model.most_similar('강의', topn=30))
+
+
+#model = doc2vec_model
+model = Doc2Vec.load('./doc2vec/9.model')
+print("================= new1 ===================")
 print(model.most_similar('강', topn=30))
 print(model.most_similar('싸강', topn=30))
-print(model.most_similar('범위', topn=30))
-print(model.most_similar('오프라인', topn=30))
+print(model.most_similar('강의', topn=30))
 # print(model.most_similar(positive=['학점'], negative=['F']))
 
--- jaebal/tuning.py
+++ jaebal/tuning.py
@@ -5,7 +5,7 @@ import csv
 
 preprocessed_texts = []
 cnt = 0
-f = open('3_tunning_shuffled_pre_review_rhino_1203.csv', 'r')
+f = open('3_tunning_shuffled_pre_review_rhino_1203_add2000.csv', 'r')
 for line in f.readlines():
     oneline = line.replace('숙,제', ',숙제,')
     oneline = oneline.replace('기,말', ',기말,')
@@ -13,10 +13,11 @@ for line in f.readlines():
     oneline = oneline.replace('기,대', ',기대,')
     oneline = oneline.replace('기,초', ',기초,')
     oneline = oneline.replace('알,채', ',안,채')
-    oneline = oneline.replace('안채,워', ',안,채우')
+    oneline = oneline.replace('안채,워', ',안,채우,')
     oneline = oneline.replace('재수,강', ',재수강,')
     oneline = oneline.replace('싸,강', ',싸강,')
     oneline = oneline.replace('인,강', ',싸강,')
+    oneline = oneline.replace('연,강', ',연강,')
     oneline = oneline.replace('강,의', ',강의,')
     oneline = oneline.replace('빡,세', ',빡세,')
     oneline = oneline.replace('빡,쎄', ',빡세,')
@@ -41,7 +42,17 @@ print(cnt)
 from gensim.models.doc2vec import Doc2Vec, TaggedDocument
 documents = [TaggedDocument(doc, [i]) for i, doc in enumerate(preprocessed_texts)]
 
-doc2vec_model = Doc2Vec(documents, vector_size=300, window=10, min_count=70, workers=4, negative=10)
+
+'''
+doc2vec_model = Doc2Vec(documents, vector_size=300, window=10, min_count=70, workers=4)
 doc2vec_model.save('./doc2vec/8.model')
+'''
 
+doc2vec_model = Doc2Vec(documents, vector_size=300, window=10, min_count=70, workers=4)
+doc2vec_model.save('./doc2vec/add1.model')
 
+doc2vec_model = Doc2Vec(documents, vector_size=300, window=10, min_count=50, workers=4)
+doc2vec_model.save('./doc2vec/add2.model')
+
+doc2vec_model = Doc2Vec(documents, vector_size=300, window=10, min_count=100, workers=4)
+doc2vec_model.save('./doc2vec/add3.model')
